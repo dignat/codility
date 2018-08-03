ef325ab735a1703e770785c17d664fe85929c233
Denise <dignat:yahoo.com>
2018-08-03 18:56:31 +0100

    array solutions

---
+++ solution/Solutions.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Solution;
+
+
+class Solutions {
+
+	public function ciclycRotation($array, $k) {
+
+		$temp = array_fill(0, count($array) -1, null);
+		for($i =0; $i < count($array); $i++) {
+			//echo ($i+$k)%count($array);
+			// echo count($array);
+			$temp[($i+$k)%count($array)] = $array[$i];
+		}
+
+		return $temp;
+	}
+
+	public function nonduplicate($a) {
+
+		foreach (array_count_values($a) as $key => $val) {
+			if ($val == 1) {
+				return $key;
+			}
+		}
+	}
+
+
+}
